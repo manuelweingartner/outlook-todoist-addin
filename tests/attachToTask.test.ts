c82725cf32f59fe7d0591edb0c353382efe94b14
Manuel Weingartner <manuel.weingartner@gmx.ch>
2026-06-26 15:23:22 +0200

    Attach-Orchestrierung: .eml bauen, Groesse pruefen, hochladen, kommentieren

---
+++ tests/attachToTask.test.ts
@@ -0,0 +1,18 @@
+import { emlBlobFor, MAX_BYTES } from "../src/lib/attachToTask";
+import { MailData } from "../src/lib/emlBuilder";
+
+const mail: MailData = {
+  subject: "Re: Angebot / Offerte",
+  from: "a@b.ch", to: [], cc: [], date: "Mon, 01 Jan 2026 10:00:00 +0000",
+  htmlBody: "<p>x</p>", attachments: [],
+};
+
+test("MAX_BYTES ist 25 MB", () => {
+  expect(MAX_BYTES).toBe(25 * 1024 * 1024);
+});
+
+test("emlBlobFor erzeugt .eml-Dateinamen aus bereinigtem Betreff", () => {
+  const { blob, fileName } = emlBlobFor(mail);
+  expect(fileName).toBe("Re_ Angebot _ Offerte.eml");
+  expect(blob.type).toBe("message/rfc822");
+});
